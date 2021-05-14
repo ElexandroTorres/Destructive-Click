93be8353ff4b18844630ad4e62ff3cb2da2ee6ee
Elexandro Torres <elexandro.torres@gmail.com>
2021-05-13 21:10:43 -0300

    Spawn de objetos em um determinado intervalo

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> objects;
+    private float spawnRate = 1.0f;
+
+    void Start()
+    {
+        StartCoroutine(SpawnObject());
+    }
+
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnObject()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, objects.Count);
+            Instantiate(objects[index]);
+        }
+    }
+}
